be58234b241f6afb390e3eeb5fa5b61fd9dd6520
Ying Wang <ying29.wang@outlook.com>
2025-02-06 12:25:31 -0800

    add eventListener

--- week5/js/main.js
+++ week5/js/main.js
@@ -1,7 +1,7 @@
-const radiusP = document.querySelector('#radius');
-const resultP = document.querySelector('#result');
+/*const radiusP = document.querySelector('#radius');
+const resultP = document.querySelector('#result');*/
 
-function getNumber() {
+/*function getNumber() {
     let radius = prompt('Enter the radius of the circle: ');
     radiusP.innerText += radius;
     return radius;
@@ -16,21 +16,41 @@ function calculateArea() {
         const area = Math.PI * Math.pow(number, 2).toFixed(2);
         return area;
     }
-}
+}*/
 
-//const result = calculateArea();
-//console.log(result);
-//resultP.innerText += result;
+// const result = calculateArea();
+// console.log(result);
+// resultP.innerText += result;
 
-let shoppingItems = ["bread", "cheese", "green pepper"];
-const shoppingList = document.querySelector('.shopping');
-console.log(shoppingList);
-function displayShoppingList() {
-    for (let i = 0; i < shoppingItems.length; i++) {
-        //console.log(shoppingItems[i]);
-        const item = document.createElement('li');
-        item.innerText = shoppingItems[i];
-        shoppingList.appendChild(item);
-    }
-}
-displayShoppingList();
+// let shoppingItems = ["bread", "cheese", "green pepper"];
+// const shoppingList = document.querySelector('.shopping');
+// console.log(shoppingList);
+// function displayShoppingList() {
+//     for (let i = 0; i < shoppingItems.length; i++) {
+//         //console.log(shoppingItems[i]);
+//         const item = document.createElement('li');
+//         item.innerText = shoppingItems[i];
+//         shoppingList.appendChild(item);
+//     }
+// }
+// displayShoppingList();
+
+// function changeListMarker() {
+//     shoppingList.setAttribute('style', 'list-style-type: square');
+//     shoppingList.classList.add('shoppingList');
+// }
+// changeListMarker();
+
+// const btn = document.querySelector('#updateImage');
+// function changeButtonText() {
+//     if (btn.innerText === 'clicked') {
+//         btn.innerText = 'click me';
+//     }
+//     else {
+//         btn.innerText = 'clicked';
+//     }
+// }
+// //btn.addEventListener('click', changeButtonText);
+// btn.addEventListener('click', changeButtonText, {once: true});
+
+
