fd511b631c4853a1b70445121d9fe35fb48b2cbc
Ying Wang <ying29.wang@outlook.com>
2025-02-06 11:53:20 -0800

    add items in the shopping list

--- week5/js/main.js
+++ week5/js/main.js
@@ -13,11 +13,24 @@ function calculateArea() {
         return 'Error: it is not a number';
     }
     else {
-        const area = Math.PI * Math.pow(number, 2).toFixed(2);  
+        const area = Math.PI * Math.pow(number, 2).toFixed(2);
         return area;
     }
 }
 
-const result = calculateArea();
-console.log(result);
-resultP.innerText += result;
+//const result = calculateArea();
+//console.log(result);
+//resultP.innerText += result;
+
+let shoppingItems = ["bread", "cheese", "green pepper"];
+const shoppingList = document.querySelector('.shopping');
+console.log(shoppingList);
+function displayShoppingList() {
+    for (let i = 0; i < shoppingItems.length; i++) {
+        //console.log(shoppingItems[i]);
+        const item = document.createElement('li');
+        item.innerText = shoppingItems[i];
+        shoppingList.appendChild(item);
+    }
+}
+displayShoppingList();
