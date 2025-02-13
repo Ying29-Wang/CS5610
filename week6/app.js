d03715c83d457a33344cf10dfcb9bcca1e6e5a68
Ying Wang <ying29.wang@outlook.com>
2025-02-13 11:53:18 -0800

    creating a module and access the module

--- week6/app.js
+++ week6/app.js
@@ -1,19 +1,23 @@
-const fs = require('fs');
-console.log(fs);
+// const fs = require('fs');
+// console.log(fs);
 
-fs.writeFile('data.txt', 'Hello, Ying!', (err) => {
-    if (err) {
-        console.log('write fail');
-    }else{
-        console.log('write success');
-        fs.readFile('data.txt', 'utf8', (err, data) => {
-            if(err){
-                console.log('read fail');
-            }
-            else{
-                console.log(data);
-            }
-        });
-    }
-});
+// fs.writeFile('data.txt', 'Hello, Ying!', (err) => {
+//     if (err) {
+//         console.log('write fail');
+//     }else{
+//         console.log('write success');
+//         fs.readFile('data.txt', 'utf8', (err, data) => {
+//             if(err){
+//                 console.log('read fail');
+//             }
+//             else{
+//                 console.log(data);
+//             }
+//         });
+//     }
+// });
 
+const logger = require("./logger.js");
+console.log(logger);
+logger.log();
+console.log(logger.version);
