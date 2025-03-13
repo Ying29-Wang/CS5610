e58f86377b019337eda6421fa8d26fe06d860f35
Ying Wang <ying29.wang@outlook.com>
2025-03-13 11:46:54 -0700

    add Header and add app name by passing a prop

--- week9/my-react-app/src/App.jsx
+++ week9/my-react-app/src/App.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import Header from './components/Header';
 
 export default function App() {
   const appName = "My Awesome App"
   return <div>
-    <h1>Welcome to {appName}</h1>
+    <Header myAppName={appName} />
   </div>;
 }
---
+++ week9/my-react-app/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Header({myAppName}) {
+    console.log(myAppName);
+    return (
+        <div>
+            <header>
+            <h1>Welcome to {myAppName}</h1>
+            </header>
+        </div>
+    );
+}
