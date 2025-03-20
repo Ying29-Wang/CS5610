13abae7be1a123b174042dd52505f22c83ee63ac
Ying Wang <ying29.wang@outlook.com>
2025-03-20 11:35:11 -0700

    add delete task function and show content if no tasks left

--- week9/my-react-app/src/components/TasksList.jsx
+++ week9/my-react-app/src/components/TasksList.jsx
@@ -20,14 +20,23 @@ export default function TasksList() {
         },
     ]);
 
-return (
-    <> Tasks List
-        <ul>
-            {tasks.map((task) => {
-                return <Task key={task.id} taskObj={task} />;
-            })}
-        </ul>
-   
-    </>
-)
+    function deleteTask(deletedId) {
+        // setTasks(tasks.filter((task) => task.id !== taskId));
+        console.log("delete pressed", deletedId);
+        const newArray = tasks.filter((task) => task.id !== deletedId);
+        setTasks(newArray);
+    }
+
+    return tasks.length === 0 ? (
+        <p>No tasks to show</p>
+    ) : (
+        <> Tasks List
+            <ul>
+                {tasks.map((task) => {
+                    return <Task key={task.id} taskObj={task} onDelete={deleteTask} />;
+                })}
+            </ul>
+
+        </>
+    )
 }
