49480637b5b1d4cc04dd7b99fd4e9e611f1c5481
Ying Wang <ying29.wang@outlook.com>
2025-03-20 12:39:49 -0700

    add delete from server

--- week9/my-react-app/src/App.jsx
+++ week9/my-react-app/src/App.jsx
@@ -1,14 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from './components/Header';
 import TasksList from './components/TasksList';
 import AddTask from './components/AddTask';
 
 export default function App() {
+  const [tasksFromServer, setTasksFromServer] = useState([]);
+  // getting data from server
+  async function fetchData() {
+    try {
+      const response = await fetch("http://localhost:5001/tasks");
+      if (!response.ok) {
+        throw new Error("fetch failed");
+      } else {
+        const data = await response.json();
+        setTasksFromServer(data);
+        console.log(data);
+      }
+  } catch (error) {
+    console.log(error);
+  }
+  }
+  
+  useEffect(() => {   
+    fetchData();
+  }
+  , []);
   const appName = "My Awesome App"
   
   return <div className="appContainer">
     <Header myAppName={appName} />
     <AddTask />
-    <TasksList />   
+    <TasksList  tasks={tasksFromServer}/>   
   </div>;
 }
--- week9/my-react-app/src/components/TasksList.jsx
+++ week9/my-react-app/src/components/TasksList.jsx
@@ -1,30 +1,22 @@
 import React, { useState } from "react";
 import Task from "./Task";
 
-export default function TasksList() {
-    const [tasks, setTasks] = useState([
-        {
-            id: 1,
-            title: "Review week 9 material",
-            date: "June 4th at 1 pm",
-        },
-        {
-            id: 2,
-            title: "Do quiz 9",
-            date: "June 4th at 6 pm",
-        },
-        {
-            id: 3,
-            title: "Work on assignment 2",
-            date: "June 5th at 8 am",
-        },
-    ]);
+export default function TasksList( {tasks} ) {
+    // const [tasks, setTasks] = useState([]);
 
-    function deleteTask(deletedId) {
+    async function deleteTask(deletedId) {
         // setTasks(tasks.filter((task) => task.id !== taskId));
-        console.log("delete pressed", deletedId);
-        const newArray = tasks.filter((task) => task.id !== deletedId);
-        setTasks(newArray);
+        // console.log("delete pressed", deletedId);
+        // const newArray = tasks.filter((task) => task.id !== deletedId);
+        // setTasks(newArray);
+        try {
+            // 1. delete from the server
+            await fetch(`http://localhost:5001/tasks/${deletedId}`, {
+            method: "DELETE",
+        })} catch (error) {
+            console.log(error);
+        }
+        console.log("task deleted");
     }
 
     return tasks.length === 0 ? (
